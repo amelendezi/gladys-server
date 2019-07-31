7e0a3adb4929ee69c1980a411e093ce0a061791e
Alberto Melendez <alberto.melendez@afas.nl>
2019-07-31 23:19:23 +0200

    Cleanup template project stuff to a simple dummy query handler

---
+++ Gladys.Web/Controllers/QueryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gladys.Web.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class QueryController : ControllerBase
+  {
+    // GET api/values
+    [HttpGet("{queryName}")]
+
+    public ActionResult<string> Get(string queryName)
+    {
+      return "Some default json";
+    }
+  }
+}
