028b148730c17e37b189a472d036f9e0f8474545
Alberto Melendez <amelendez.es@gmail.com>
2019-08-04 21:53:01 +0200

    Created residence model.

---
+++ Gladys.Api/Model/ResidenceModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Gladys.Api.Model
+{
+  public class ResidenceModel
+  {
+    public ResidenceModel(Guid id, string alias, string residenceOwnerName, string residentName)
+    {
+      Id = id;
+      Alias = alias;
+      ResidenceOwnerName = residenceOwnerName;
+      ResidentName = residentName;
+    }
+
+    public Guid Id { get; }
+    
+    public string Alias { get; }
+
+    public string ResidenceOwnerName { get; }
+
+    public string ResidentName { get; }
+  }
+}
