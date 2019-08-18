d7038713926df496738661950bdc132e9cbb83d9
Alberto Melendez <amelendez.es@gmail.com>
2019-08-18 21:24:40 +0200

    Added basic test project and suite for the connection string parser

---
+++ Gladys.Repository.Tests/MySql/MySqlConnectionStringParserTests.cs
@@ -0,0 +1,51 @@
+using Gladys.Repository.MySql;
+using NUnit.Framework;
+
+namespace Gladys.Repository.Tests.MySql
+{
+    [TestFixture]
+    public class MySqlConnectionStringParserTests
+    {
+        [Test]
+        public void On_Load_ReturnsCorrectConfig()
+        {
+            string filePath = @"..\..\..\..\Gladys.AppData\dbconn.txt";
+
+            var result = MySqlConnectionStringParser.Load(filePath);
+            
+            Assert.AreEqual(expected: "gladys", actual: result.User);
+            Assert.AreEqual(expected: "gladyspass", actual: result.Password);
+            Assert.AreEqual(expected: "localhost", actual: result.Server);
+            Assert.AreEqual(expected: "gladysdb", actual: result.Database);
+        }
+        
+        [Test]
+        public void On_Parse_ReturnsCorrectConfig()
+        {
+            string sample = "Uid=gladys;Pwd=gladyspass;Server=localhost;Database=gladysdb";
+
+            var result = MySqlConnectionStringParser.Parse(sample);
+            
+            Assert.AreEqual(expected: "gladys", actual: result.User);
+            Assert.AreEqual(expected: "gladyspass", actual: result.Password);
+            Assert.AreEqual(expected: "localhost", actual: result.Server);
+            Assert.AreEqual(expected: "gladysdb", actual: result.Database);
+        }
+
+        [Test]
+        public void On_SerializeToConnectionString_ReturnsCorrectString()
+        {
+            var sample = new MySqlConnectionConfig()
+            {
+                User = "gladys",
+                Password = "gladyspass",
+                Server = "localhost",
+                Database = "gladysdb"
+            };
+            
+            string result = MySqlConnectionStringParser.SerializeToConnectionString(sample);
+            
+            Assert.AreEqual(expected: "Uid=gladys;Pwd=gladyspass;Server=localhost;Database=gladysdb", actual: result);
+        }
+    }
+}
